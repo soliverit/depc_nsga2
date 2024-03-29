27a7f586d80455bdfe9a971dca5bb447050a0632
soliverit <soliver.i.t@gmail.com>
2024-03-29 01:57:54 +0000

    ./nsga2_community.py: add recurrent processing loop

--- nsga2_community.py
+++ nsga2_community.py
@@ -1,8 +1,8 @@
 ### Includes ###
 ## Native 
 from os.path				import isfile
+from time					import time
 ## Project
-from lib.retrofit_nsga2		import RetrofitNSGA2
 from lib.nsga2_community	import NSGA2Community
 from lib.building_set		import BuildingSet
 #############################
@@ -10,19 +10,32 @@ from lib.building_set		import BuildingSet
 #############################
 # Parse
 params			= NSGA2Community.ParseCMD()
+
 # Verify
 dataPath		= "./data/" + params["dataCode"] + ".csv"
 if not isfile(dataPath):
 	print("Data path: " + dataPath + " not found")
 	exit()
+del params["dataCode"]
 #############################
 #    The actual example     #
 #############################
 # Load data
 buildings	= BuildingSet.LoadDataSet(dataPath).getByRatings(["G", "F", "E"])
-del params["dataCode"]
-community	= NSGA2Community(buildings, "shoe", 
-				partitions=params["partitions"], 
-				threadCount=params["threads"], 
-				flags=RetrofitNSGA2.ParamsToFlagString(params))
-community.run()
+inequality	= buildings.toRatingDifference(params["targetRating"])
+counter 	= 0
+startTime	= time() 
+while counter < params["recurrentSteps"]:
+	community	= NSGA2Community(buildings, "shoe", 
+					partitions=params["partitions"], 
+					threadCount=params["threads"],
+					inequality=inequality,
+					flags=params,
+					stateLabel="_" + str(counter))
+	community.run()
+	buildings	= community.results
+	buildings.getScoreStruct("BEST_TEAM_STATE").print()
+	params["stateIdentifier"] = "BEST_TEAM_STATE"
+	buildings.shuffle()
+	counter += 1
+print("Community process time: %s" %(time() - startTime))
