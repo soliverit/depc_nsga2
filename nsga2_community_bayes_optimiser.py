44a61a5e579126003a3015307ed2eb090a4f35b9
soliverit <soliver.i.t@gmail.com>
2024-03-31 21:04:05 +0100

    Renmaed bayes optimiser files

--- nsga2_community_bayes_optimiser.py
+++ nsga2_community_bayes_optimiser.py
@@ -2,7 +2,7 @@
 ## Native 
 from bayes_opt				import BayesianOptimization
 ## Project
-from lib.retrofit_nsga2		import RetrofitNSGA2
+from lib.nsga2_community	import NSGA2Community
 from lib.building_set		import BuildingSet
 from lib.cost_problem		import CostProblem
 
@@ -11,44 +11,41 @@ from lib.cost_problem		import CostProblem
 ##
 ### Define stuff ###
 # Get command line parameters: call "python bayes_optimiser.py -h for details"
-params				= RetrofitNSGA2.ParseCMD()
+params				= NSGA2Community.ParseCMD()
+params["stateIdentifier"] = "BEST_TEAM_STATE"
 # Load the dataset. Data resides in ./data_samples, the --code parameter defines the file name. E.g mid = mid.csv
 buildings			= BuildingSet.LoadDataSet("./data/" + params["dataCode"] + ".csv")
+del params["dataCode"]
 # Define the problem
 problem				= CostProblem(buildings)
 problem.inequality	= problem.buildings.toRatingDifference("D")
 # Define the GA. We'll reuse this instead of creating a fresh model for every evaluation
-retrofitGA	= RetrofitNSGA2(problem,
-	generations=params["generations"],
-	crossover=True,	# It tunes crossover params
-	crossoverETA=params["crossoverETA"],
-	crossoverProb=params["crossoverProb"],
-	mutationETA=params["mutationETA"],
-	population=params["population"],
-	children=params["children"],
-	verbose=params["verbose"]
-)
 
 ### Define evaluation function
 def optimise(crossoverETA, crossoverProb, mutationETA):
-	global retrofitGA	# Yes, globals are dirty! Very convnient here
+	global params, buildings	# Yes, globals are dirty! Very convnient here
+	params["crossoverETA"]	= crossoverETA
+	params["crossoverProb"]	= crossoverProb
+	params["mutationETA"]	= mutationETA
 	## Reconfigure the GA
-	retrofitGA.crossoverETA		= crossoverETA
-	retrofitGA.crossoverProb	= crossoverProb
-	retrofitGA.mutationETA		= mutationETA
+	retrofitGA	= NSGA2Community(
+		buildings,
+		inequality=problem.inequality,	# the CostProblem def of this is redudant for NSGA2Community
+		partitions=params["partitions"],
+		threadCount=params["threads"],
+		flags=params
+	)
 	# Run the GA
 	retrofitGA.run()
 	# Return the results. The bayes library maximises, so return a negative
-	results						= retrofitGA.getResult()
-	retrofitGA.printResults()
-	return - int(round(results["cost"] / results["points"]))
+	return - retrofitGA.results.getScoreStruct("BEST_TEAM_STATE").ratio
 
 ### Let's do it! ###
 # Define the NSGA parameters that'are being tuned. These are for PM crossover and SBX mutation
 bounds			= {
 	"crossoverProb":(0.7,0.9),
-	"crossoverETA": (10, 25),
-	"mutationETA": (5, 10)
+	"crossoverETA": (5, 12),
+	"mutationETA": (1, 5)
 }
 # Define the optimiser
 optimiser	= BayesianOptimization(
@@ -58,7 +55,7 @@ optimiser	= BayesianOptimization(
 )
 # Run the optimiser. There's no minimise function
 optimiser.maximize(
-	init_points=5,
+	init_points=20,
 	n_iter=200,
 )
 
