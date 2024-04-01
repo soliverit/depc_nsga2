e0582d99cbe8901938b479fba71878e13fb81d19
soliverit <soliver.i.t@gmail.com>
2024-04-01 21:29:03 +0100

    Updated the lib/estimators/__init__.py module file. Used for dynamic constuctor loading in EstimatorBase

--- lib/estimators/__init__.py
+++ lib/estimators/__init__.py
@@ -1,1 +1,2 @@
-from lib.estimators.xgboost_estimator import XGBoostEstimator
+from lib.estimators.xgboost_estimator	import XGBoostEstimator
+from lib.estimators.gbdt_estimator		import GBDTEstimator
